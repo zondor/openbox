a736f5f4e7c79564ba3fb50fac097e2931d5655a
Denis [zorge] <zondor.index@gmail.com>
2015-02-28 22:08:00 +0200

    Updated install

--- inst/install.sh
+++ inst/install.sh
@@ -1,11 +1,12 @@
 set -e
 
-echo "\033[0;34mCloning Open box install...\033[0m"
-hash git >/dev/null 2>&1 && env git clone --depth=1 https://github.com/zondor/openbox.git ~/.openbox-inst || {
-  echo "git not installed"
-  exit
-}
-
+if [ ! -d ~/.openbox-inst ]; then
+	echo "\033[0;34mCloning Open box install...\033[0m"
+	hash git >/dev/null 2>&1 && env git clone --depth=1 https://github.com/zondor/openbox.git ~/.openbox-inst || {
+	  echo "git not installed"
+	  exit
+	}
+fi
 bash ~/.openbox-inst/setup.sh
 
 
